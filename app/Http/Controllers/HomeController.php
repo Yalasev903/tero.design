e8ebebf9b72810ad2e60663b1fc2e6e8674ce667
Yalasev903 <slasev903@gmail.com>
2025-06-04 13:05:17 +0300

    home_projects_grid.project_id

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,9 +10,15 @@ class HomeController extends Controller
     public function index()
     {
         $projects_grid_raw = DB::table('home_projects_grid')
-            ->orderBy('row_number')
-            ->orderBy('col_number')
-            ->get();
+        ->leftJoin('projects', 'home_projects_grid.project_id', '=', 'projects.id')
+        ->select(
+            'home_projects_grid.*',
+            'projects.title as project_title',
+            'projects.text2 as project_text2'
+        )
+        ->orderBy('row_number')
+        ->orderBy('col_number')
+        ->get();
 
         $projects_grid = [];
         foreach ($projects_grid_raw as $item) {
@@ -20,19 +26,20 @@ class HomeController extends Controller
                 'project_id' => $item->project_id,
                 'media' => $item->media,
                 'is_mobile' => (bool)$item->is_mobile,
-                'title' => '',
-                'text2' => '',
+                'title' => $item->project_title ?? '',
+                'text2' => $item->project_text2 ?? '',
             ];
         }
 
-        // Сортируем строки и колонки по ключу, чтобы не было дыр
+        // Сортировка
         ksort($projects_grid);
         foreach ($projects_grid as &$row) {
             ksort($row);
-            $row = array_values($row); // индексы 0,1,2,...
+            $row = array_values($row);
         }
         unset($row);
 
         return view('home', compact('projects_grid'));
     }
+
 }
