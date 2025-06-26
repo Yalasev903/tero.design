45bf047a887cafbbb6d4e764742504b4ab3d2496
Yalasev903 <slasev903@gmail.com>
2025-06-26 19:03:28 +0300

    create сессия

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -3,5 +3,16 @@
 </template>
 
 <script setup>
-</script>
+import axios from 'axios'
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+setInterval(async () => {
+  try {
+    await axios.get('/api/admin/me')
+  } catch {
+    router.push('/login')
+  }
+}, 1000 * 60 * 5) // каждые 5 минут
+</script>
