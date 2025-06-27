5045cf574fbe6160e5466814f4a8893a6140968d
Yalasev903 <slasev903@gmail.com>
2025-06-27 15:38:02 +0300

    временно ключ в файл tinymce2

--- resources/js/components/admin/TinyEditor.vue
+++ resources/js/components/admin/TinyEditor.vue
@@ -36,7 +36,7 @@ const tinyKey = 'zz5yzfkvutel7xxsey78ithzogwrudzczlqmwlyft73cupew'
       menubar: true,
       branding: false,
       toolbar_mode: 'sliding',
-      plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount emoticons',
+      plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount emoticons',
       toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | emoticons charmap | link image media table | fullscreen code preview',
       content_style: `body { font-size: ${fontSize}; }`
     }"
