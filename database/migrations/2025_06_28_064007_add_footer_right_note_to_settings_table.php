34c6e8acc404af5fa46da88bc5679f7a67162ae9
Yalasev903 <slasev903@gmail.com>
2025-06-28 09:58:32 +0300

    make columns footer_right_note for settings2

--- database/migrations/2025_06_28_064007_add_footer_right_note_to_settings_table.php
+++ database/migrations/2025_06_28_064007_add_footer_right_note_to_settings_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('settings', function (Blueprint $table) {
+        Schema::table('tbl_settings', function (Blueprint $table) {
             $table->string('footer_right_note')->nullable();
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('settings', function (Blueprint $table) {
+        Schema::table('tbl_settings', function (Blueprint $table) {
             $table->string('footer_right_note')->nullable();
         });
     }
