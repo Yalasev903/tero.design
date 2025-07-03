3a312be55225c0a6e8e9bb598de22d36b80931bd
Yalasev903 <slasev903@gmail.com>
2025-07-03 09:22:09 +0300

    настройка сетки в HomeGrid кнопка Edit

--- resources/js/views/admin/HomeGrid.vue
+++ resources/js/views/admin/HomeGrid.vue
@@ -947,7 +947,7 @@ onMounted(() => {
 .delete-icon {
   position: absolute;
   top: 6px;
-  box-shadow: 0 2px 6px rgba(0,0,0,0.13);
+  box-shadow: 0 2px 6px rgba(114, 113, 113, 0.13);
   font-size: 16px;
   padding: 4px;
   color: #fff;
@@ -962,7 +962,7 @@ onMounted(() => {
 }
 
 .edit-icon {
-  right: 34px;
+  right: 8px;
 }
 
 .delete-icon {
