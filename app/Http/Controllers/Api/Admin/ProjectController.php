7bb5aecb6977c1d0cdf42c88556ea9561440437c
Yalasev903 <slasev903@gmail.com>
2025-07-14 15:08:45 +0300

    🐛 Fix: добавлен use ZipArchive для распаковки VR-архива

--- app/Http/Controllers/Api/Admin/ProjectController.php
+++ app/Http/Controllers/Api/Admin/ProjectController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use ZipArchive;
 
 class ProjectController extends Controller
 {
