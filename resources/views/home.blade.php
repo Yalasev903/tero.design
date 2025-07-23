ac220358371001a6f84b883da8a4ae1eb26ff91f
Yalasev903 <slasev903@gmail.com>
2025-07-23 19:11:26 +0300

    первый этап страницы с подгрузкой на главной 3 перенос из шаблона в отдельный файл скрипт сетки 6

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -557,5 +557,7 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     </script>
 
-    <script src="/js/grid-loader.js"></script>
+
+    <script src="{{ asset('js/header.min.js?v=5.92') }}"></script>
+        <script src="/js/grid-loader.js"></script>
 @endsection
